6549da1acc19d8731250bb10ce24d81cbabccfde
stalkrbeardedler18 <rumlerguoyi@hotmail.com>
2025-08-15 05:59:39 -0500

    feat: initial commit for server/index.js

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+// MongoDB connection
+mongoose.connect('mongodb://localhost:27017/code-simplicity-visualizer', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
