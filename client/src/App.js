651ca67dfdfd5dbe4fb309cd247c1ed7b818fc82
stalkrbeardedler18 <rumlerguoyi@hotmail.com>
2025-08-15 05:59:44 -0500

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Code Simplicity Visualizer</h1>
+      <p>Welcome to the Code Simplicity Visualizer!</p>
+    </div>
+  );
+}
+
+export default App;
